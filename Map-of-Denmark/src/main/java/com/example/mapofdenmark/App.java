b50860091d76cea8da1088d36b076b140a8d6958
niko391a <69769473+niko391a@users.noreply.github.com>
2024-03-12 12:39:04 +0100

    Attempt at loading .fxml file into scene

--- Map-of-Denmark/src/main/java/com/example/mapofdenmark/App.java
+++ Map-of-Denmark/src/main/java/com/example/mapofdenmark/App.java
@@ -1,6 +1,9 @@
 package com.example.mapofdenmark;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -11,14 +14,23 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         //String filename = "data/denmark.osm.zip.obj";
-        String path = System.getProperty("user.dir"); // gets which directory the project is placed
-        String filename = path+"\\data\\small.osm";
-        System.out.println(path);
-        var model = Model.load(filename);
-        var view = new View(model, primaryStage);
-        new Controller(model, view);
-    }
+//        String path = System.getProperty("user.dir"); // gets which directory the project is placed
+//        String filename = path+"\\data\\small.osm";
+//        System.out.println(path);
+//        var model = Model.load(filename);
+//        var view = new View(model, primaryStage);
+//        new Controller(model, view);
 
+//        String path = System.getProperty("src\\main\\resources\\com\\example\\mapofdenmark\\GUI.fxml"); // gets which directory the project is placed
+//        String filename = path+"\\data\\small.osm";
+//        System.out.println(path);
+        String fxmlPath = "/com/example/mapofdenmark/GUI.fxml";
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     public Object getGreeting() {
         return null;
     }
