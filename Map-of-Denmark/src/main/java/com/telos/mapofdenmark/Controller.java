37917e7679327ab186bdccfb586128b56a4e6e38
mnla <mnla@itu.dk>
2024-04-04 17:42:38 +0200

    Added a Custom start up screen
    Added a Custom start up screen
    Added ability to start from default file
    Added ability to start from custom file

--- Map-of-Denmark/src/main/java/com/telos/mapofdenmark/Controller.java
+++ Map-of-Denmark/src/main/java/com/telos/mapofdenmark/Controller.java
@@ -101,14 +101,14 @@ public class Controller {
         if (themeToggleBtn.isSelected()) {
             themeToggleBtn.getStyleClass().remove("root-light");
             themeToggleBtn.getStyleClass().add("root-dark");
-            System.out.println("Dark theme");
             Dark = true;
+            view.togglecolor(Dark);
             view.redraw();
         } else {
             themeToggleBtn.getStyleClass().remove("root-dark");
             themeToggleBtn.getStyleClass().add("root-light");
             Dark = false;
-            view.togglecolor(Dark);
+
             view.redraw();
         }
     }
