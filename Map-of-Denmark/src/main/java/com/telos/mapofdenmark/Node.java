cead4dc9ab55ba8ab62ae9d30d83848d6d626572
niko391a <69769473+niko391a@users.noreply.github.com>
2024-04-05 15:30:11 +0200

    now properly retrieves lat and lon from adress

--- Map-of-Denmark/src/main/java/com/telos/mapofdenmark/Node.java
+++ Map-of-Denmark/src/main/java/com/telos/mapofdenmark/Node.java
@@ -1,10 +1,19 @@
 package com.telos.mapofdenmark;
 
-public class Node {
+import java.io.Serializable;
+
+public class Node implements Serializable {
     double lat, lon;
 
     public Node(double lat, double lon) {
         this.lat = lat;
         this.lon = lon;
+    }
+
+    public double getLat(){
+        return this.lat;
+    }
+    public double getLon(){
+        return this.lon;
     }
 }
