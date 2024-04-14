9c82f7ec93dd52b5be4b4ae68b753493f15870a1
niko391a <69769473+niko391a@users.noreply.github.com>
2024-04-14 19:48:34 +0200

    Modified BST to accomodate coordinates as key

--- Map-of-Denmark/src/main/java/com/telos/mapofdenmark/KDTreeClasses/BST.java
+++ Map-of-Denmark/src/main/java/com/telos/mapofdenmark/KDTreeClasses/BST.java
@@ -1,18 +1,23 @@
 package com.telos.mapofdenmark.KDTreeClasses;
 
 import java.util.*;
-
+// Inspiration to KDTree from https://www.geeksforgeeks.org/search-and-insertion-in-k-dimensional-tree/
+// We also utilized code from chapter 3 in Algorithms 4 by Wayne and Sedgewick ch 3.2
 public class BST<Key extends Comparable<Key>, Value>
 {
     private Node root; // root of BST
     private class Node {
-        private Key key;
+        private Key key; // Primitive key is no longer needed in KDTree as coordinates will be used as key instead.
+        double x, y; // Coordinates of the point which will be used for the line that will be drawn to create half planes
         private Value val;
         private Node left, right; // links to the left and right subtrees
         private int size; // number of nodes in the subtree rooted here
-        public Node(Key key, Value val, int size)
+        public Node(double x, double y, Value val, int size)
         {
-            this.key = key;
+            // Constructor has been changed from a normal BST to accomodate coordinates
+            //this.key = key;
+            this.x = x;
+            this.y = y;
             this.val = val;
             this.size = size;
         }
@@ -30,32 +35,82 @@ public class BST<Key extends Comparable<Key>, Value>
         // Checks if the BST is empty
         return size() == 0;
     }
-    public void put(Key key, Value val) {
+    public void put(double x, double y, Value val) {
         // Public method to insert a key-value pair; updates if key exists
-        root = put(root, key, val);
+        // Put method arguments change to acommodate nodes new attributes
+        root = put(root, x, y, val, 0);
     }
-    private Node put(Node x, Key key, Value val) {
+    private Node put(Node x, double xCoord, double yCoord, Value val, int depth) {
         // Recursively inserts a key-value pair into the BST
         // Change key’s value to val if key in subtree rooted at x.
         // Otherwise, add new node to subtree associating key with val.
-        if (x == null) return new Node(key, val, 1);
-        int cmp = key.compareTo(x.key);
-        if (cmp < 0) x.left = put(x.left, key, val);
-        else if (cmp > 0) x.right = put(x.right, key, val);
+//        if (x == null) return new Node(key, val, 1);
+//        int cmp = key.compareTo(x.key);
+//        if (cmp < 0) x.left = put(x.left, key, val);
+//        else if (cmp > 0) x.right = put(x.right, key, val);
+//        else x.val = val;
+//        x.size = size(x.left) + size(x.right) + 1;
+//        return x;
+
+        // New put adapted to KDTree
+        if (x == null) return new Node(xCoord, yCoord, val, 1);
+        // Perform comparison based on the determined axis
+        int cmp;
+        // Determine the axis of comparison based on current depth (0 for x, 1 for y)
+        int axis = depth % 2;
+        // The following two conditionals is used to determine whether to divide half-plane vertically or horizontally
+        if (axis == 0) cmp = Double.compare(xCoord, x.x); // Compare x-coordinates if axis is 0
+        else cmp = Double.compare(yCoord, x.y); // Compare y-coordinates if axis is 1
+
+        // Recursive insert: navigate left or right based on comparison result
+        // If the compared value is less, go to the left subtree
+        if (cmp < 0) x.left = put(x.left, xCoord, yCoord, val, depth + 1);
+        // If the compared value is greater, go to the right subtree
+        else if (cmp > 0) x.right = put(x.right, xCoord, yCoord, val, depth + 1);
+        // If the point exactly matches (in the compared dimension), update the value.
         else x.val = val;
+
+        // Update the size of the subtree rooted at this node
         x.size = size(x.left) + size(x.right) + 1;
         return x;
     }
-    public Value get(Key key) {
+    public Value get(double x, double y){
+        return get(x, y, 0);
+    }
+    private Value get(double xCoord, double yCoord, int depth) {
         // Retrieves the value associated with a given key
         Node x = root;
+        int xDepth = depth;
         while (x != null)
         {
-            int cmp = key.compareTo(x.key);
-            if (cmp < 0) x = x.left;
-            else if (cmp > 0) x = x.right;
-            else return x.val;
+            int axis = depth % 2;
+            // Vertical line
+            if(axis == 0){
+                int cmp = Double.compare(yCoord, x.x);
+                if (cmp < 0){
+                    //depth++; depth should not be called from conditional as an asymmetric depth could happen
+                    x = x.left;
+                }
+                else if (cmp > 0) {
+                    //depth++;
+                    x = x.right;
+                }
+                else return x.val;
+            } // Horizontal line
+            else {
+                int cmp = Double.compare(xCoord, x.y);
+                if (cmp < 0){
+                    //depth++;
+                    x = x.left;
+                }
+                else if (cmp > 0) {
+                    //depth++;
+                    x = x.right;
+                }
+                else return x.val;
+            }
         }
+        xDepth++; // Increment depth at each step to alternate comparison axis
         return null;
     }
     public Key min() {
