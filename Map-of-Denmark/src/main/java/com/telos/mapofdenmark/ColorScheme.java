794825fbfb47913f7e816c103fad00162a3183fe
niko391a <69769473+niko391a@users.noreply.github.com>
2024-05-03 11:00:50 +0200

    Update ColorScheme.java

--- Map-of-Denmark/src/main/java/com/telos/mapofdenmark/ColorScheme.java
+++ Map-of-Denmark/src/main/java/com/telos/mapofdenmark/ColorScheme.java
@@ -25,7 +25,7 @@ public class ColorScheme implements Serializable{
             }
             return Color.web(hex);
         } catch (NullPointerException e) {
-            if (!type.isEmpty()) System.out.println(type);
+            if (!type.isEmpty()) System.out.println("missing color on: " + type);
             return getDefaultColor(dark);
         }
 
@@ -43,53 +43,92 @@ public class ColorScheme implements Serializable{
     public void defaultScheme(){
         colors.clear();
 
-        // Relations
-        // Natural
+        // Place
         colors.put("islet", "#f2efe9");
-
+        darkColors.put("islet", "#464646");
         colors.put("island", "#f2efe9");
+        darkColors.put("island", "#3d3d3d");
+        colors.put("hamlet", "#f2efe9");
+        darkColors.put("hamlet", "#323232");
+        colors.put("archipelago", "#f2efe9");
+        darkColors.put("archipelago", "#292929");
 
+        // Natural
         colors.put("peninsula","#f2efe9");
+        darkColors.put("peninsula", "#464646");
+        colors.put("wetland", "#8cbf80");
+        darkColors.put("wetland", "#5e8e5b");
+        colors.put("heath", "#d6d99f");
+        darkColors.put("heath", "#b0b283");
+        colors.put("scrub", "#c9d8ac");
+        darkColors.put("scrub", "#a3b292");
+        colors.put("water", "#aad3df");
+        darkColors.put("water", "#495f66");
+        colors.put("beach", "#fff1bb");
+        darkColors.put("beach", "#e0c77a");
+        colors.put("bare_rock", "#c3c3c3");
+        darkColors.put("bare_rock", "#a6a6a6");
+        colors.put("scree", "#e4dcd4");
+        darkColors.put("scree", "#c5bdb5");
+        colors.put("wood", "#9dca8a");
+        darkColors.put("wood", "#748d62");
+        colors.put("bay", "#aad3df");
+        darkColors.put("bay", "#aad3df");
+        colors.put("grassland", "#8cbf80");
+        darkColors.put("grassland", "#5e8e5b");
 
-        colors.put("hamlet", "#f5dcba");
+        // Landuse
+        colors.put("meadow", "#8cbf80");
+        darkColors.put("meadow", "#5e8e5b");
+        colors.put("forest", "#7aa66f");
+        darkColors.put("forest", "#5c7a58");
+        colors.put("grass", "#8cbf80");
+        darkColors.put("grass", "#5e8e5b");
+        colors.put("farmland", "#8cbf80");
+        darkColors.put("farmland", "#5e8e5b");
+        colors.put("quarry", "#c4c2c2");
+        darkColors.put("quarry", "#9b9999");
+        colors.put("farmyard", "#efd5b3");
+        darkColors.put("farmyard", "#d4b295");
+        colors.put("industrial", "#ebdbe9");
+        darkColors.put("industrial", "#b7a9b5");
+        colors.put("plant_nursery", "#8cbf80");
+        darkColors.put("plant_nursery", "#5e8e5b");
 
-
-        // Leisure
-        colors.put("park", "#c8facc");
-
-        colors.put("marina","#90c5ee");
-
-        colors.put("golf_course", "#d9d0c9");
-
-        // Amenity
-        colors.put("parking", "#eeeeee");
-
-        colors.put("university", "#ffffe5");
 
         // Building
         colors.put("apartments", "#d8d0c9");
-
+        darkColors.put("apartments", "#9b9087");
         colors.put("house", "#d8d0c9");
-
+        darkColors.put("house", "#9b9087");
         colors.put("residential", "#d8d0c9");
-
+        darkColors.put("residential", "#9b9087");
         colors.put("dormitory", "#d8d0c9");
-
+        darkColors.put("dormitory", "#9b9087");
         colors.put("office", "#d8d0c9");
-
+        darkColors.put("office", "#9b9087");
         colors.put("school", "#d8d0c9");
-
+        darkColors.put("school", "#9b9087");
         colors.put("college", "#d8d0c9");
-
+        darkColors.put("college", "#9b9087");
         colors.put("hospital", "#d8d0c9");
-
+        darkColors.put("hospital", "#9b9087");
         colors.put("retail", "#d8d0c9");
-
+        darkColors.put("retail", "#9b9087");
         colors.put("square", "#d8d0c9");
-
+        darkColors.put("square", "#9b9087");
         colors.put("hotel", "#d8d0c9");
-
+        darkColors.put("hotel", "#9b9087");
         colors.put("yes", "#d8d0c9");
+        darkColors.put("yes", "#9b9087");
+        colors.put("farm", "#d8d0c9");
+        darkColors.put("farm", "#9b9087");
+        colors.put("terrace", "#d8d0c9");
+        darkColors.put("terrace", "#9b9087");
+        colors.put("semidetached_house", "#d8d0c9");
+        darkColors.put("semidetached_house", "#9b9087");
+        colors.put("detached", "#d8d0c9");
+        darkColors.put("detached", "#d8d0c9");
 
         //Roads
         colors.put("default", "#000000");
@@ -140,101 +179,6 @@ public class ColorScheme implements Serializable{
         darkColors.put("footway", "#FFFFFF");
         colors.put("path", "#808B96");
         darkColors.put("path", "#FFFFFF");
-
-        // NATURALS
-        colors.put("fell","#48a160");
-        colors.put("grassland","#f2efe9");
-        colors.put("heath","#eaf0d6");
-        colors.put("moor","#45ed7d");
-        colors.put("scrub","#93ad6a");
-        colors.put("scrubbery","#93ad6a");
-        colors.put("tree","#188f40");
-        colors.put("tree_row","#188f40");
-        colors.put("tree_stump","#5c2606");
-        colors.put("tundra","#48a160");
-        colors.put("wood","#188f40");
-        colors.put("bay","#90c5ee");
-        colors.put("beach","#f2db68");
-        colors.put("blowhole","#f2db68");
-        colors.put("cape","#f2db68");
-        colors.put("coastline", "#ecf0ec"); //original:#ecf0ec pink:#ff73e8
-        colors.put("crevasse","#68e2f2");
-        colors.put("geyser","#acb4b5");
-        colors.put("glacier","#68e2f2");
-        colors.put("hot_spring","#a8d5ed");
-        colors.put("isthmus","#45ed7d");
-        colors.put("mud","#332702");
-        colors.put("shoal","#f2db68");
-        colors.put("spring","#7daaff");
-        colors.put("strait","#7daaff");
-        colors.put("water","#90c5ee");
-        colors.put("wetland","#637796");
-        colors.put("bare_rock","#adadad");
-        colors.put("arete","#6f7070");
-        colors.put("blockfield","#6f7070");
-        colors.put("cave_entrance","#6f7070");
-        colors.put("cliff","#989a9c");
-        colors.put("dune","#f2bf33");
-        colors.put("earth_bank","#5c361d");
-        colors.put("fumarole","#5c361d");
-        colors.put("gully","#5c361d");
-        colors.put("hill","#6d8f29");
-        colors.put("peak","#d9fcfb");
-        colors.put("ridge","#d9fcfb");
-        colors.put("saddle","#d9fcfb");
-        colors.put("sand","#d6c64f");
-        colors.put("scree","#6f7070");
-        colors.put("sinkhole","#6f7070");
-        colors.put("stone","#6f707");
-        colors.put("volcano","#453724");
-
-        // Landuse
-
-        // Below is the exact value for all landuses:
-        // commercial
-        // construction
-        // education
-        // fairground
-        // industrial
-        // residential
-        // retail
-        // institutional
-        // aquaculture
-        // allotments
-        // farmland
-        // farmyard
-        // paddy
-        // animal_keeping
-        // flowerbed
-        colors.put("forest","#9dca8a");
-        // greenhouse_horticulture
-        colors.put("meadow", "#ceecb1");
-        // orchard
-        // plant_nursery
-        // vineyard
-        // basin
-        // salt_pond
-        // brownfield
-        colors.put("cemetery","#abccb0");
-        // depot
-        // garages
-        colors.put("grass","#68bf60");
-        // greenfield
-        // landfill
-        // military
-        // port ----------------- should not be drawn
-        // quarry
-        // railway
-        // recreation_ground
-        // religious
-        // village_green
-        // winter_sports
-
-        // Leisure
-        colors.put("garden","#d3efb6");
-        colors.put("playground","#dffce2");
-        //colors.put("slipway",""); ---------------- should not be drawn
-        //colors.put("marina",""); ----------------- should not be drawn
 
     }
 
