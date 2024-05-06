1a479483b10c5c0dfc4155b7643e36f960f5775f
DragonFlyersx <mnla@itu.dk>
2024-05-06 21:00:00 +0200

    UP?

--- Map-of-Denmark/src/main/java/com/telos/mapofdenmark/Shortest_Route/EdgeWeightedDigraph.java
+++ Map-of-Denmark/src/main/java/com/telos/mapofdenmark/Shortest_Route/EdgeWeightedDigraph.java
@@ -2,25 +2,48 @@ package com.telos.mapofdenmark.Shortest_Route;
 
 import java.io.Serializable;
 
+/**
+ *  This is the class for the weighted directed graph used int the implemetation of the shortespath algorithm.
+ *  It consists of vertices and directed edges with weights.
+ */
 public class EdgeWeightedDigraph implements Serializable {
     private final int V;
     private final Bag<DirectedEdge>[] adj;
 
+    /**
+     * This is the contrustor for the weighted directed graph. That initializes the graph to the size of vv
+     * @param V the size of the weighted directed graph( the number of vertices)
+     */
     public EdgeWeightedDigraph(int V){
         this.V = V;
         adj = (Bag<DirectedEdge>[]) new Bag[V];
         for (int v = 0; v < V; v++)
             adj[v] = new Bag<DirectedEdge>();
     }
+
+    /**
+     *  Adds a directed edge to the graph
+     * @param e the directeddd edge to add
+     */
     public void addEdge(DirectedEdge e)
     {
         int v = e.from();
         adj[v].add(e);
     }
+    /**
+     * Returns an iterable containing the directed edges adjacent to vertex 'v'.
+     *
+     * @param v is the vertes for which adjacent edges are attach.
+     * @return an iterable containing the directed edges adjacent to vertex 'v'
+     */
     public Iterable<DirectedEdge> adj(int v)
     { return adj[v]; }
 
-
+    /**
+     * Returns the number of vertices in the graph.
+     *
+     * @return the number of vertices in the graph
+     */
     public int V() {
         return V;
     }
--- Map-of-Denmark/src/main/java/com/telos/mapofdenmark/Shortest_Route/SP.java
+++ Map-of-Denmark/src/main/java/com/telos/mapofdenmark/Shortest_Route/SP.java
@@ -1,26 +1,43 @@
 package com.telos.mapofdenmark.Shortest_Route;
-import com.telos.mapofdenmark.Model;
 import com.telos.mapofdenmark.Node;
 
 import java.util.ArrayList;
 import java.util.Stack;
 
+/**
+ * The SP class represents a shortest path algorithm implementation for finding the shortest path
+ * in a weighted directed graph from a start node as an index to a goal node. It employs Dijkstra's algorithm
+ * with the addition of a heuristic function to optimize the search.
+ */
 public class SP
 {
     private DirectedEdge[] edgeTo;
     private double[] distTo;
     private IndexMinPQ<Double> pq;
     private boolean vehicle;
-    private int Goal;
     private ArrayList<Node> nodes;
+
+
+
+    /**
+     * Initializes the shortest path algorithm from the start node as index 's' to the goal node 'goal'
+     * in the given weighted directed graph 'G'. The 'vehicle' parameter indicates whether the
+     * path calculation is for a bike or car The 'nodes' parameter provides the list of nodes
+     * necessary for heuristic calculation.
+     *
+     * @param G       the weighted directed graph
+     * @param s       the start/source node index
+     * @param goal    the goal node index
+     * @param vehicle indicates if the path calculation is for bike or a car
+     * @param Nodes   list of nodes for heuristic calculation
+     */
     public SP(EdgeWeightedDigraph G, int s, int goal, boolean vehicle, ArrayList<Node> Nodes)
     {
         this.nodes = Nodes;
         this.vehicle = vehicle;
-        this.Goal = goal;
         edgeTo = new DirectedEdge[G.V()];
         distTo = new double[G.V()];
-        pq = new IndexMinPQ<Double>(G.V());
+        pq = new IndexMinPQ<>(G.V());
         for (int v = 0; v < G.V(); v++)
             distTo[v] = Double.POSITIVE_INFINITY;
         distTo[s] = 0.0;
@@ -31,8 +48,13 @@ public class SP
                 relax(e, goal);
         }
     }
-    private void relax(DirectedEdge e, int goal)
-    {
+
+    /**
+     * A function for Relaxing and edge when Dijkstra has passed it.
+     * @param e is the Directed edge from the graphs adjacent edges.
+     * @param goal Is the end point( end nodes index value/id)
+     */
+    private void relax(DirectedEdge e, int goal){
         int v = e.from(), w = e.to();
         double newDistToW = distTo[v] + e.weight(vehicle);
         if (distTo[w] > newDistToW) {
@@ -44,9 +66,11 @@ public class SP
         }
     }
 
-    public double distTo(int v)
-    { return distTo[v]; }
-
+    /**
+     * Gets the path from start to finish and makes an iterable of Directededges that summarise the path.
+     * @param v End node as index
+     * @return And itereable list of Directed edges from the start point to end
+     */
     public Iterable<DirectedEdge> pathTo(int v)
     {
         Stack<DirectedEdge> path = new Stack<DirectedEdge>();
@@ -55,6 +79,13 @@ public class SP
         return path;
     }
 
+    /**
+     *  A Helper function for the A* heuristic. This helper function calculateds the distance from the node that is currently being explored to the end node in a
+     *  birds as distance as crows fly. This helps to get the shortest path quicker.
+     * @param v the current node that is being explored
+     * @param goal The destination for the pathfinding algorith the place we want to end up.
+     * @return this returns the distance that has been calculated.
+     */
     private double heuristic(int v, int goal){
        Node currentNode = nodes.get(v);
        Node goalNode = nodes.get(goal);
